bec7ff4f6a058a41e9571a8922478c9fa25fc2a2
Soham Bagchi <soham.bagchi_ug22@ashoka.edu.in>
2021-04-12 10:17:21 +0530

    commented out sys_date

--- syscall.c
+++ syscall.c
@@ -157,7 +157,7 @@ extern int sys_unlink(void);
 extern int sys_wait(void);
 extern int sys_write(void);
 extern int sys_uptime(void);
-extern int sys_date(void);
+//extern int sys_date(void);
 
 static int (*syscalls[])(void) = {
 [SYS_fork]    sys_fork,
@@ -181,7 +181,7 @@ static int (*syscalls[])(void) = {
 [SYS_link]    sys_link,
 [SYS_mkdir]   sys_mkdir,
 [SYS_close]   sys_close,
-[SYS_date]    sys_date,
+//[SYS_date]    sys_date,
 };
 
 void
